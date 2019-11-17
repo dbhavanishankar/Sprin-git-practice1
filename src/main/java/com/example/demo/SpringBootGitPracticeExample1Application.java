2b7c600c63a5a44b50e18ea33fb9012c1e5e46f0
anil <anil@gmail.com>
2019-11-17 21:29:30 +0530

    adding all files into local repository

---
+++ src/main/java/com/example/demo/SpringBootGitPracticeExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitPracticeExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitPracticeExample1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/MyRestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+
+	@GetMapping("/wish")
+	public String wish() {
+		String msg = "welcome to Spring boot with Git Example";
+		return msg;
+
+	}
+}
