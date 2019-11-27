0bae3fbc3efac099e0d092a35e52d6ab704851ea
dbhavanishankar <49371226+dbhavanishankar@users.noreply.github.com>
2019-11-27 23:47:27 +0530

    Update MyRestController.java

--- src/main/java/com/example/demo/controller/MyRestController.java
+++ src/main/java/com/example/demo/controller/MyRestController.java
@@ -8,7 +8,7 @@ public class MyRestController {
 
 	@GetMapping("/wish")
 	public String wish() {
-		String msg = "welcome to Spring boot with Git Examples bhavani sha dakkathya";
+		String msg = "welcome to Spring boot with Git Examples bhavani shan dakkathya";
 		return msg;
 		
 
