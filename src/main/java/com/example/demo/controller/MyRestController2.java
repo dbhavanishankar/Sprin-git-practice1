796daf7f805b90125d4d9b47ea719c880a0e6b24
anil <anil@gmail.com>
2019-11-27 19:02:35 +0530

    adding comments

--- src/main/java/com/example/demo/controller/MyRestController2.java
+++ src/main/java/com/example/demo/controller/MyRestController2.java
@@ -9,6 +9,11 @@ public class MyRestController2 {
 	@GetMapping("/wish")
 	public String wish() {
 		String msg="hi welcome to RestController2";
+		//cooment 1
+		//cooment 2
+		//cooment 3
+		//cooment 4
+		//cooment 5
 		return msg;
 	}
 
