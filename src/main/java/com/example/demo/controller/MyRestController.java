27da77e71db9f43c7f8b03f426b49e39003a2a33
anil <anil@gmail.com>
2019-12-05 23:26:24 +0530

    removing 4

--- src/main/java/com/example/demo/controller/MyRestController.java
+++ src/main/java/com/example/demo/controller/MyRestController.java
@@ -8,7 +8,7 @@ public class MyRestController {
 
 	@GetMapping("/wish")
 	public String wish() {
-		String msg = "welcome to Spring boot with Git Examples bhavani latest124";
+		String msg = "welcome to Spring boot with Git Examples bhavani latest12";
 		return msg;
 		
 
